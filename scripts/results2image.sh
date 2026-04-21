f9bcd07de50a7ebc239fd5f8264d0f736f945666
Haruaki Tamada <tamada@users.noreply.github.com>
2026-04-21 17:55:17 +0900

    Update dependencies in Cargo.toml and improve script permissions and functionality

--- scripts/results2image.sh
+++ scripts/results2image.sh
@@ -2,12 +2,13 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <base>"
     exit 1
 fi
-$base=$1
-mkdir -p $1/{images,results,tables}
+base=$(cd $1; pwd)
+
+mkdir -p $base/{images,results,tables}
 for i in cosine dice levenshtein lcs jaccard euclidean simpson weighted-jaccard
 do
     echo "Processing $i..."
-    cp $base/$i/results.json $base/results/$i.json
-    python scripts/results2table.py $base/results/$i.json > $base/tables/$i.csv
-    heatman -p 10 --order $i/../headers.csv --dest $base/images/$i.png $base/tables/$i.csv
+    cp $base/$i/results.csv $base/results/$i.csv
+    python scripts/results2table.py $base/results/$i.csv > $base/tables/$i.csv
+    heatman -p 10 --order $base/../headers.csv --dest $base/images/$i.png $base/tables/$i.csv
 done
